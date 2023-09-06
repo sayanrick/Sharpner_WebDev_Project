53f14b9445f9c5f3c54d0dc31a8b3ce9424041ad
Sayan Das <80049309+sayanrick@users.noreply.github.com>
2023-09-06 16:49:38 +0530

    Delete Expense_Tracker_NodeJS_Project/Hashing_Password directory

--- Expense_Tracker_NodeJS_Project/Hashing_Password/util/database.js
+++
@@ -1,8 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('expense_tracker_app', 'root', 'root', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
-
-module.exports = sequelize;
